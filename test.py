df5feaca60480bb28fd04193c8c0c60f026d9a82
vipulsolanki <solankivipul2498@gmail.com>
2023-01-20 11:34:00 +0530

    initial commit

---
+++ test.py
@@ -0,0 +1,31 @@
+
+# num=int(input("Enter number:"))
+# k=num*2
+# for i in range(1,num+1):
+#     for j in range(1,k+1):
+#         print(" ",end="")
+#     k=k-2
+#     for j in range(1,i+1):
+#         print(" *",end=" ")
+#     print("")
+
+
+
+
+n=int(input("Enter Number:"))
+k=n*2
+for i in range(1,n+1):
+    for k in range(1,k+1):
+        print(" ",end="")
+    k=k-2
+    for j in range(1,i+1):
+        print("*",end=' ')
+    print("")
+
+
+
+
+
+
+
+
